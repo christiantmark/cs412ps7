a96b1c3eb4d5546a7ebd3461e3146ea4646163cf
Christian Mark <christiantmark@gmail.com>
2023-04-30 19:40:30 -0400

    angular

---
+++ src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+providedIn: 'root'
+})
+export class ApiService {
+private SERVER_URL = "http://localhost:3000";
+constructor(private httpClient: HttpClient) { }
+public get(){ 
+  return this.httpClient.get(this.SERVER_URL); 
+  } 
+  }
+}
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
+
+//   constructor() { }
+// }
